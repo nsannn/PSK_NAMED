cc9b7665f5f467611989d6eccb57ec985da073ba
Artiom <artiomgarbul@gmail.com>
2026-05-28 12:47:40 +0300

    Small fix to Ticket validation page

--- frontend/src/TicketValidation.jsx
+++ frontend/src/TicketValidation.jsx
@@ -169,7 +169,7 @@ export default function TicketValidation(){
                 </>
             ):(
                 <div id="ticket_info_container" className={`align_column ${resultType}`}>
-                    <span id="status">{user.role!="Customer" ? "Please sign in to validate tickets":"Not authorized to use this page"}</span>
+                    <span id="status">Please sign in into authorized account to validate tickets</span>
                 </div>
             )}
         </div>
